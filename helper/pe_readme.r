f4792f9d2af6b75c245d8a3ca7b278591409ac32
Shane Drabing <shane.drabing@gmail.com>
2022-01-28 02:36:20 -0700

    zero doesn't count

--- helper/pe_readme.r
+++ helper/pe_readme.r
@@ -10,7 +10,7 @@ fnames <- unlist(sapply(dir_full("src"), dir_full))
 mat <- do.call(rbind, strsplit(fnames, "/"))[, -1]
 
 length <- 65
-dat <- table(mat[!grepl("_fail", mat[, 2]), 1])
+dat <- table(mat[!grepl("_fail", mat[, 2]), 1]) - 1
 tbl <- round(length * dat / 772)
 tbl
 
