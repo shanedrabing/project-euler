be87feeb595e8ce6a8558d8e1509d8acd42e5db5
Shane Drabing <shane.drabing@gmail.com>
2022-08-26 13:11:45 -0600

    pe 4 python shortened

--- src/py/pe_0004.py
+++ src/py/pe_0004.py
@@ -1,6 +1,3 @@
-import operator
-
-
 # FUNCTIONS
 
 
@@ -9,22 +6,14 @@ def is_palindromic(n):
     return str_n == str_n[::-1]
 
 
-def reverse_number(n):
-    m = 0
-    while (n != 0):
-        m *= 10
-        m += n % 10
-        n //= 10
-    return m
-
-
 # SCRIPT
 
 
 if __name__ == "__main__":
-    set_prods = set()
-    for a in range(100, 1000):
-        for b in range(a, 1000):
-            set_prods.add(a * b)
+    set_prods = {
+        a * b
+        for a in range(100, 1000)
+        for b in range(a, 1000)
+    }
 
     print(max(filter(is_palindromic, set_prods)))
