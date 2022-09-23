61642c48a53b54fd7130856ef6f1a2e5a84bd9bf
Shane Drabing <shane.drabing@gmail.com>
2022-09-23 16:57:25 -0600

    problem 10 javascript

---
+++ src/js/pe_0010.js
@@ -0,0 +1,39 @@
+// SCRIPT
+
+
+{
+    // initialize vairables
+    size = 2000000
+    half = Math.floor(size / 2)
+    prime = Array(size).fill(false)
+
+    // set two as prime, begin sum
+    prime[2] = true
+    gross = 2
+
+    // all odds are potential primes
+    for (i = 3; i < size; i += 2) {
+        prime[i] = true
+    }
+
+    // disregard multiples of primes
+    i = 3
+    for (; i < half; i += 2) {
+        if (prime[i]) {
+            gross += i
+            for (j = i * 2; j < size; j += i) {
+                prime[j] = false
+            }
+        }
+    }
+
+    // continue the sum
+    for (; i < size; i += 2) {
+        if (prime[i]) {
+            gross += i
+        }
+    }
+
+    // print out
+    console.log(gross);
+}
